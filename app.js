6900f329386c51347b7d11bcc5fdb1d74828462a
pablete18 <pabloealcaraz@gmail.com>
2023-07-20 00:39:11 -0300

    feat: se agrega flex-box en dispositivos mobile

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ app.listen(PORT, ()=> console.log(`servidor corriendo en http://localhost:${PORT
 app.use(express.static('public'))
 
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')))
-
+app.get('/registro',(req,res)=>res.sendFile(path.join(__dirname,'views','register.html')))
 
